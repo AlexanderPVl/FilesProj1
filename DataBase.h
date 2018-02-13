667eb1ce548209ff7aefe3777f868c7f77fe1713
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-02-13 10:12:32 +0200

    Update DataBase.h

--- DataBase.h
+++ DataBase.h
@@ -3,6 +3,21 @@
 
 #include "file.h"
 
+_Data CreateDataBase(int Columns)
+{
+	_Data Dat1 = (_Data)malloc(sizeof(string));
+
+	return Dat1;
+}
+
+void FreeData(DataBase *DB)
+{
+	int i = 0;
+	for (; i < DB->Lins; i++)
+		free(DB->Adres[i]);
+	free(DB->Adres);
+}
+
 void PrintLine(int Num)
 {
 
