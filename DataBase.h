ee420df15eda9df46f16733d137c9aae768fd3a4
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-02-13 10:47:22 +0200

    Update DataBase.h

--- DataBase.h
+++ DataBase.h
@@ -1,24 +1,42 @@
 #include <stdio.h>
 #include <string.h>
-
 #include "file.h"
 
-_Data CreateDataBase(int Columns)
+typedef char** _Data;
+
+typedef struct
+{
+	_Data Adres;
+	int LinCount;
+} DataBase;
+
+_Data Create_Data_Base(int Columns)
 {
 	_Data Dat1 = (_Data)malloc(sizeof(string));
 
 	return Dat1;
 }
 
-void FreeData(DataBase *DB)
+void Free_Data(DataBase *DB)
 {
 	int i = 0;
-	for (; i < DB->Lins; i++)
+	for (; i < DB->LinCount; i++)
 		free(DB->Adres[i]);
 	free(DB->Adres);
 }
 
-void PrintLine(int Num)
+int Print_DB_Line(int Num, DataBase *DB)
 {
+	if (Num>DB->LinCount){
+		printf("ERROR: Wrong number\n");
+		return 0;
+	}
+	printf("String number %d:\n%s", Num, DB->Adres[Num - 1]);
+	return 1;
+}
 
+int Add_ToDB_Strings(DataBase *DB, int Count)
+{
+	DB->LinCount += Count;
+	DB->Adres = (_Data)realloc(DB->Adres, DB->LinCount*sizeof(string));
 }
