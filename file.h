e9e3d9843415d954ab48b30fa5169f0e4f942832
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-02-13 09:53:30 +0200

    Create file.h

---
+++ file.h
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef char* string;
+
+string EnterStr(string Dep)
+{
+	int i = 0, len = 50;
+	char c;
+	string Str = (string)malloc(10 * sizeof(char));
+	printf("Please, enter your string\n");
+	while ((c = getchar()) != '\n')
+	{
+		if (i >= len)
+		{
+			len += 50;
+			Str = (string)realloc(Str, len*sizeof(char));
+		}
+		Str[i++] = c;
+	}
+	Str[i] = '\0';
+	printf("\n");
+	free(Dep);
+	return Str;
+}
+
+void PrintStrin(string Str)
+{
+	printf("Your string:\n(%s)\n\n", Str);
+}
+
+int PrintToFile(string NOF, string Str)
+{
+	int i = 0;
+	char c;
+	if (fopen(NOF, "r") == NULL){
+		printf("ERROR: File \"%s\" does not exist, do you want to create if?\n1 - yes, 0 - no\n\n", NOF);
+		while ((c = getchar()) != '1' && c != '0');
+		c -= '0';
+		if (c == 0)
+			return 0;
+	}
+	FILE *f = fopen(NOF, "w");
+	if (f == NULL)
+	{
+		printf("File \"%s\" is not created\n", NOF);
+		return 0;
+	}
+	else
+		printf("File \"%s\" is created\n", NOF);
+	if (fprintf(f, Str) < 0)
+		printf("Operation failed\n");
+	else
+		printf("Operation succeeded");
+	while (Str[i] != '\0')
+		fputc((int)Str[i++], f);
+	fclose(f);
+}
